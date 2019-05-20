89b86816680e967c8be04aa7e8e2d8d11581dfe9
tabatista <tabatista@indracompany.com>
2019-05-20 20:37:43 -0300

    anotacoes

--- src/main/java/com/igti/filme/controller/Principal.java
+++ src/main/java/com/igti/filme/controller/Principal.java
@@ -4,14 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-//representa uma API essa anotacao
+//representa uma API (recebe as chamadas HTTPs)
 @RestController
-//toda requisicao que tiver / vai cair nessa classe
+//toda requisicao que tiver / vai cair nessa classe (mapeia as requisições HTTPs em métodos)
 @RequestMapping("/")
 public class Principal {
 	
 	//define metodo get
-	@RequestMapping(method= RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public String saudacao() {
 		return "Hello World";
 	};

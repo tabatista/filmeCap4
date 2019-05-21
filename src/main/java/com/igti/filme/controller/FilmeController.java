bd5349e6675fe113186aebd1b98416a1b121a14f
tabatista <tabatista@indracompany.com>
2019-05-21 20:35:51 -0300

    aula 4.5 - find by id

--- src/main/java/com/igti/filme/controller/FilmeController.java
+++ src/main/java/com/igti/filme/controller/FilmeController.java
@@ -2,10 +2,12 @@ package com.igti.filme.controller;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,4 +35,16 @@ public class FilmeController {
 		return new ResponseEntity<>(filmes, HttpStatus.OK); //OK corresponde ao status 200
 	}
 	
+	@RequestMapping(path = "{id}", method = RequestMethod.GET)
+	public ResponseEntity<Filme> obterById(@PathVariable String id){
+		
+		Optional<Filme> filmeOptional = filmeRepository.findById(id);
+		
+		//verifica se o filme existe
+		if(!filmeOptional.isPresent()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} 		
+		////acessa pela URL http://localhost:8080/filmes/id
+		return new ResponseEntity<>(filmeOptional.get(), HttpStatus.OK);		
+	}
 }

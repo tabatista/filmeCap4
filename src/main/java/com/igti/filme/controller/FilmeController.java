e8bc63d4a4a9321d8d1c3cdad0081eedfc74ddcb
tabatista <tabatista@indracompany.com>
2019-05-21 21:06:33 -0300

    aula 4.8 - patch

--- src/main/java/com/igti/filme/controller/FilmeController.java
+++ src/main/java/com/igti/filme/controller/FilmeController.java
@@ -22,72 +22,109 @@ import com.igti.filme.vo.FilmeRequest;
 @RestController
 @RequestMapping("/filmes")
 public class FilmeController {
-	
-	//injeta o repositorio de filmes do Mongo
+
+	// injeta o repositorio de filmes do Mongo
 	@Autowired
 	private FilmeRepository filmeRepository;
 
-	//o ResponseEntity encapsula um response que eh apresentado como json
-	//eh possivel  passar no response o status da resposta
-	//alem das informacoes
+	// o ResponseEntity encapsula um response que eh apresentado como json
+	// eh possivel passar no response o status da resposta
+	// alem das informacoes
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<Filme>> listar(){
-		
-		List<Filme> filmes = filmeRepository.findAll();		
-		
-		//acessa pela URL http://localhost:8080/filmes
-		return new ResponseEntity<>(filmes, HttpStatus.OK); //OK corresponde ao status 200
+	public ResponseEntity<List<Filme>> listar() {
+
+		List<Filme> filmes = filmeRepository.findAll();
+
+		// acessa pela URL http://localhost:8080/filmes
+		return new ResponseEntity<>(filmes, HttpStatus.OK); // OK corresponde ao status 200
 	}
-	
-	//path = parametro que vira da url e sera mapeado no parâmetro id do método
+
+	// path = parametro que vira da url e sera mapeado no parâmetro id do método
 	@RequestMapping(path = "{id}", method = RequestMethod.GET)
-	public ResponseEntity<Filme> obterById(@PathVariable String id){
-		
+	public ResponseEntity<Filme> obterById(@PathVariable String id) {
+
 		Optional<Filme> filmeOptional = filmeRepository.findById(id);
-		
-		//verifica se o filme existe
-		if(!filmeOptional.isPresent()) {
+
+		// verifica se o filme existe
+		if (!filmeOptional.isPresent()) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		} 		
-		//acessa pela URL http://localhost:8080/filmes/id
-		return new ResponseEntity<>(filmeOptional.get(), HttpStatus.OK);		
+		}
+		// acessa pela URL http://localhost:8080/filmes/id
+		return new ResponseEntity<>(filmeOptional.get(), HttpStatus.OK);
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
-	//pega as informacoes do corpo da requisicao com o @RequestBody - o corpo do request sera mapeado no parametro
-	//com o @Valid os dados do request serão validados.
-	public ResponseEntity<Void> criar(@RequestBody @Valid FilmeRequest request){
+	// pega as informacoes do corpo da requisicao com o @RequestBody - o corpo do
+	// request sera mapeado no parametro
+	// com o @Valid os dados do request serão validados.
+	public ResponseEntity<Void> criar(@RequestBody @Valid FilmeRequest request) {
 		Filme filme = new Filme();
 		filme.setTitulo(request.getTitulo());
 		filme.setDiretor(request.getDiretor());
 		filme.setGenero(request.getGenero());
 		filme.setAno(request.getAno());
-		
+
 		filmeRepository.save(filme);
-		
-		//acessa pela URL http://localhost:8080/filmes/ (COM O BODY PREENCHIDO - POSTMAN)
-		//status 201
+
+		// acessa pela URL http://localhost:8080/filmes/ (COM O BODY PREENCHIDO -
+		// POSTMAN)
+		// status 201
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
-	
+
 	@RequestMapping(path = "{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> alterar(@PathVariable String id, @RequestBody @Valid FilmeRequest request){
-		
+	public ResponseEntity<Void> alterar(@PathVariable String id, @RequestBody @Valid FilmeRequest request) {
+
 		Optional<Filme> filmeOptional = filmeRepository.findById(id);
-		
-		//verifica se o filme NAO existe
-		if(!filmeOptional.isPresent()) {
+
+		// verifica se o filme NAO existe
+		if (!filmeOptional.isPresent()) {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-		} 
-		
+		}
+
 		filmeOptional.get().setTitulo(request.getTitulo());
 		filmeOptional.get().setDiretor(request.getDiretor());
 		filmeOptional.get().setGenero(request.getGenero());
 		filmeOptional.get().setAno(request.getAno());
-		
+
 		filmeRepository.save(filmeOptional.get());
-		
-		//acessa pela URL http://localhost:8080/filmes/id (COM O BODY PREENCHIDO - POSTMAN)
+
+		// acessa pela URL http://localhost:8080/filmes/id (COM O BODY PREENCHIDO -
+		// POSTMAN)
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@RequestMapping(path = "{id}", method = RequestMethod.PATCH)
+	public ResponseEntity<Void> alterarCampo(@PathVariable String id, @RequestBody @Valid FilmeRequest request) {
+
+		Optional<Filme> filmeOptional = filmeRepository.findById(id);
+
+		// verifica se o filme NAO existe
+		if (!filmeOptional.isPresent()) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+
+		//verificaremos se os dados dos request existem
+		if (request.getTitulo() != null) {
+			filmeOptional.get().setTitulo(request.getTitulo());
+		}
+
+		if (request.getDiretor() != null) {
+			filmeOptional.get().setDiretor(request.getDiretor());
+		}
+
+		if (request.getGenero() != null) {
+			filmeOptional.get().setGenero(request.getGenero());
+		}
+
+		if (request.getAno() > 0) {
+			filmeOptional.get().setAno(request.getAno());
+		}
+
+		filmeRepository.save(filmeOptional.get());
+
+		// acessa pela URL http://localhost:8080/filmes/id (COM O BODY PREENCHIDO PARCIALMENTE -
+		// POSTMAN)
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 	
